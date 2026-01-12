9a715836f307568835d5803c337366e157128cff
Emiliano <ecoria543@gmail.com>
2026-01-12 08:50:40 -0300

    Fix: Move page.tsx to correct app directory

--- page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from '@/components/Hero';
-
-export default function Home() {
-    return (
-        <main className="min-h-screen bg-slate-950">
-            <Hero />
-        </main>
-    );
-}
